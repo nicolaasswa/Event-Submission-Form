b62b4a3df94db77060bc3d1dcc6d09175cd86c27
Nick Swanevelder <nickyswa@gmail.com>
2018-09-16 21:17:45 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Event-Submission-Form
---
+++ js/index.js
@@ -0,0 +1,11 @@
+//DATE TIME SELECTOR:
+$(function() {
+  $('input[name="datetimes"]').daterangepicker({
+    timePicker: true,
+    startDate: moment().startOf('hour'),
+    endDate: moment().startOf('hour').add(32, 'hour'),
+    locale: {
+      format: 'M/DD hh:mm A'
+    }
+  });
+});
